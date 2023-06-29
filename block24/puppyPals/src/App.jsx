589973ea0c1be81ba9ffede15c8fdb01be4cd1b2
Kebinsan <prokevint@gmail.com>
2023-06-28 22:44:16 -0400

    block 24 puppy pals assignment

---
+++ block24/puppyPals/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "./style.css";
+import { puppyList } from "./data.js";
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  return (
+    <div className="App">
+      <div className="puppies">
+        {puppies.map((puppy) => {
+          return (
+            <p
+              className="puppy-name"
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+      </div>
+      {featPupId && (
+        <div className="feat-pup">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
