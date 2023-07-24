579591772192fd6c94f17dbc27cc229b1c8e6a92
Kebinsan <prokevint@gmail.com>
2023-07-24 16:22:17 -0400

    select user, need to finish displaying the data

--- block26/ContactList/src/components/ContactList.jsx
+++ block26/ContactList/src/components/ContactList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import ContactRow from "./ContactRow";
+import { useEffect } from "react";
 
 const dummyContacts = [
   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
@@ -8,8 +9,23 @@ const dummyContacts = [
   { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
 ];
 
-const ContactList = () => {
+const ContactList = ({ setSelectedContactId }) => {
   const [contacts, setContacts] = useState(dummyContacts);
+  useEffect(() => {
+    async function fetchContacts() {
+      try {
+        const response = await fetch(
+          "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+        );
+        const result = await response.json();
+        setContacts(result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchContacts();
+  }, []);
+
   console.log("Contacts: ", contacts);
   return (
     <table>
@@ -25,7 +41,13 @@ const ContactList = () => {
           <td>Phone</td>
         </tr>
         {contacts.map((contact) => {
-          return <ContactRow key={contact.id} contact={contact} />;
+          return (
+            <ContactRow
+              key={contact.id}
+              contact={contact}
+              setSelectedContactId={setSelectedContactId}
+            />
+          );
         })}
       </tbody>
     </table>
